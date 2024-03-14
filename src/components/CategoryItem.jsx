86e2ab85a92ff843521db02a52fa1e7ecf19878d
Pavan-2929 <kapdiyapavan3218@gmail.com>
2024-03-14 23:21:03 +0530

    fixed suspense

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,19 +1,26 @@
-"use client";
-
 import Link from "next/link";
-import React from "react";
+import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 const CategoryItem = ({ text, param }) => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CategoryItemContent text={text} param={param} />
+    </Suspense>
+  );
+};
+
+const CategoryItemContent = ({ text, param }) => {
   const searchParams = useSearchParams();
 
   const searchItem = searchParams.get("search");
 
-  const isTrending = param === "fetchTrending" && !searchItem
+  const isTrending = param === "fetchTrending" && !searchItem;
+
   return (
     <div
       className={`hover:text-amber-500 ${
-        (searchItem === param || isTrending)
+        searchItem === param || isTrending
           ? "text-amber-500 underline underline-offset-4 decoration-2 decoration-amber-500"
           : ""
       } `}
